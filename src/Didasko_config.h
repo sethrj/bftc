d5a443d6c6ede330d4239189da9f619ec5d4f434
johnson.seth.r <johnson.seth.r@d54e0c0a-c7dd-11dd-9042-6d746b85d38b>
2009-07-04 18:25:43 +0000

    Switched to cmake, added trilinos example

---
+++ src/Didasko_config.h
@@ -0,0 +1,176 @@
+/* src/Didasko_config.h.  Generated from Didasko_config.h.in by configure.  */
+/* src/Didasko_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <assert.h> header file. */
+/* #undef HAVE_ASSERT_H */
+
+/* define if bool is a built-in type */
+#define HAVE_BOOL 
+
+/* Define to 1 if you have the <cassert> header file. */
+#define HAVE_CASSERT 1
+
+/* Define to 1 if you have the <cmath> header file. */
+#define HAVE_CMATH 1
+
+/* Define to 1 if you have the <cstdio> header file. */
+#define HAVE_CSTDIO 1
+
+/* Define to 1 if you have the <cstdlib> header file. */
+#define HAVE_CSTDLIB 1
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_AMESOS 1
+
+/* Define if want to build with didasko enabled */
+/* #undef HAVE_DIDASKO_ANASAZI */
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_AZTECOO 1
+
+/* Define if want to build with didasko enabled */
+/* #undef HAVE_DIDASKO_BELOS */
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_EPETRA 1
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_EPETRAEXT 1
+
+/* Define if want to build didasko-examples */
+#define HAVE_DIDASKO_EXAMPLES 
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_IFPACK 1
+
+/* Define if want to build with latex enabled */
+/* #undef HAVE_DIDASKO_LATEX */
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_ML 1
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_NOX 1
+
+/* Define if want to build didasko-tests */
+#define HAVE_DIDASKO_TESTS 
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_TEUCHOS 1
+
+/* Define if want to build with didasko enabled */
+/* #undef HAVE_DIDASKO_TPETRA */
+
+/* Define if want to build with didasko enabled */
+#define HAVE_DIDASKO_TRIUTILS 1
+
+/* Define if want to build examples */
+#define HAVE_EXAMPLES 
+
+/* Define if you want to build export makefiles. */
+#define HAVE_EXPORT_MAKEFILES 
+
+/* Define if you are using gnumake - this will shorten your link lines. */
+/* #undef HAVE_GNUMAKE */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <iomanip> header file. */
+#define HAVE_IOMANIP 1
+
+/* Define to 1 if you have the <iomanip.h> header file. */
+/* #undef HAVE_IOMANIP_H */
+
+/* Define to 1 if you have the <iostream> header file. */
+#define HAVE_IOSTREAM 1
+
+/* Define to 1 if you have the <iostream.h> header file. */
+/* #undef HAVE_IOSTREAM_H */
+
+/* Define if want to build libcheck */
+#define HAVE_LIBCHECK 
+
+/* Define to 1 if you have the <math.h> header file. */
+/* #undef HAVE_MATH_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* define if we want to use MPI */
+/* #undef HAVE_MPI */
+
+/* define if the compiler supports the mutable keyword */
+#define HAVE_MUTABLE 
+
+/* define if the compiler implements namespaces */
+#define HAVE_NAMESPACES 
+
+/* define if the compiler accepts the new for scoping rules */
+#define HAVE_NEW_FOR_SCOPING 
+
+/* Define if want to build nox-epetra */
+/* #undef HAVE_NOX_EPETRA */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+/* #undef HAVE_STDIO_H */
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* define if std::sprintf is supported */
+#define HAVE_STD_SPRINTF 
+
+/* define if the compiler supports Standard Template Library */
+#define HAVE_STL 
+
+/* Define to 1 if you have the <string> header file. */
+#define HAVE_STRING 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if want to build tests */
+#define HAVE_TESTS 
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if want to build epetraext-zoltan */
+/* #undef HAVE_ZOLTAN */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "trilinos-help@software.sandia.gov"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "didasko"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "didasko 9.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "didasko"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "9.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
